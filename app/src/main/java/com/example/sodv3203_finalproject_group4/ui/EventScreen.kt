afea1a62ff0ed6a9e2f14383af5945efc4c20435
BruceMaBVC <bmaka@hotmail.com>
2024-03-21 17:21:27 -0600

    commit

--- app/src/main/java/com/example/sodv3203_finalproject_group4/ui/EventScreen.kt
+++ app/src/main/java/com/example/sodv3203_finalproject_group4/ui/EventScreen.kt
@@ -3,21 +3,20 @@ package com.example.sodv3203_finalproject_group4.ui
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.sodv3203_finalproject_group4.ShoppingBuddyScreen
 import com.example.sodv3203_finalproject_group4.ui.theme.ShoppingBuddyAppTheme
 
-@Composable
-<<<<<<< HEAD
-fun EventScreen() {
-    Column (
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-=======
 fun EventScreen(userId: Int, eventId: Int) {
     var searchText by remember { mutableStateOf(TextFieldValue()) }
     Column(modifier = Modifier.padding(16.dp)) {
@@ -27,7 +26,6 @@ fun EventScreen(userId: Int, eventId: Int) {
     }
 
     Column(
->>>>>>> dd590a253b07d491ecb2ac15542f4c9073aea9d6
         modifier = Modifier.fillMaxSize()
     ) {
         Text(
